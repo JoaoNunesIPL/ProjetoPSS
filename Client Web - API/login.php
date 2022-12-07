85bdde9859d8aa75712ee2f54173b491794b232d
João Nunes <j.f.d.n2003@gmail.com>
2022-12-07 12:16:21 +0000

    Comit, will prob come back

--- Client Web - API/login.php
+++ Client Web - API/login.php
@@ -1,57 +1,57 @@
 <?php
 session_start();
+
 $username="joao";
-$password="joao";
+$password="1234";
+
 if (isset($_POST['username']) && isset($_POST['password']))
     if($username == $_POST['username'] && $password == $_POST['password']){
         echo("O username submetido foi: ".$_POST['username']."<br>");
         echo("O password submetido foi: ".$_POST['password']."<br>");
     $_SESSION['username']=$_POST['username'];
-    header('location: https://www.youtube.com/watch?v=k-UvIuFDRKc')
+    header('location: https://www.youtube.com/watch?v=k-UvIuFDRKc');
     }
     else{
-        echo("Autenticação FALHADA")
+        echo("Autenticação FALHADA");
     }
 ?>
 
 <!DOCTYPE html>
 <html>
+
+<head>
+  <link rel="stylesheet" href="./Style/styleLOGIN.css">
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+</head>
+
 <body>
 
+<div class="wrapper fadeInDown">
+  <div id="formContent">
+    <!-- Tabs Titles -->
+
+    <!-- Icon -->
+    <div class="fadeIn first">
+      <img src="./bee.jpg" id="icon" alt="User Icon" />
+    </div>
+
+    <!-- Login Form -->
     <form>
-    <!-- Email input -->
-    <div class="form-outline mb-4">
-        <input type="username" id="form1Example1" class="form-control" />
-        <label class="form-label" for="form1Example1">Username</label>
-    </div>
-
-    <!-- Password input -->
-    <div class="form-outline mb-4">
-        <input type="password" id="form1Example2" class="form-control" />
-        <label class="form-label" for="form1Example2">Password</label>
-    </div>
-
-    <!-- 2 column grid layout for inline styling -->
-    <div class="row mb-4">
-        <div class="col d-flex justify-content-center">
-        <!-- Checkbox -->
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="form1Example3" checked />
-            <label class="form-check-label" for="form1Example3"> Remember me </label>
-        </div>
-        </div>
-
-        <div class="col">
-        <!-- Simple link -->
-        <a href="#!">Forgot password?</a>
-        </div>
-    </div>
-
-    <!-- Submit button -->
-    <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+      <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
+      <input type="password" id="password" class="fadeIn third" name="login" placeholder="password">
+      <input type="submit" class="fadeIn fourth" value="Log In">
     </form>
 
+    <!-- Voltar a pagina inicial -->
+    <div id="formFooter">
+      <a class="underlineHover" href="../index.html">Pagina inicial</a>
+    </div>
+
+  </div>
+</div>
+
 </body>
-</html>
-
-
+</html> 
